ef48070c2f70887d2fa89b97ac147af541da6727
Maxim Nyanin <maximnyanin@gmail.com>
2024-10-02 00:11:55 -0400

    Create Cell class

--- main.py
+++ main.py
@@ -28,6 +28,9 @@ class Window():
 
     def draw_line(self, line, color):
         line.draw(self.__canvas, color)
+    
+    def draw_cell(self, cell, color):
+        cell.draw(self.__canvas, color)
 
 class Point():
 
@@ -44,10 +47,34 @@ class Line():
     def draw(self, canvas: Canvas, fillColor: str) -> None:
         canvas.create_line(self.p1.x, self.p1.y, self.p2.x, self.p2.y, fill=fillColor, width=2)
 
+class Cell():
+    
+    def __init__(self, x1, x2, y1, y2) -> None:
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        self._win = False
+
+    def draw(self, canvas: Canvas, color: str):
+        if self.has_left_wall:
+            Line(Point(self._x1, self._y2), Point(self._x1, self._y1)).draw(canvas, color)
+        if self.has_top_wall:
+            Line(Point(self._x1, self._y1), Point(self._x2, self._y1)).draw(canvas, color)
+        if self.has_right_wall:
+            Line(Point(self._x2, self._y1), Point(self._x2, self._y2)).draw(canvas, color)
+        if self.has_bottom_wall:
+            Line(Point(self._x2, self._y2), Point(self._x1, self._y2)).draw(canvas, color)
+
+
 def main():
     win = Window(800, 600)
-    line = Line(Point(100, 100), Point(500, 500))
-    win.draw_line(line, "red")
+    cell = Cell(200, 600, 100, 500)
+    win.draw_cell(cell, "blue")
     win.wait_for_close()
     
 if __name__ == "__main__":
